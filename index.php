d85a57ba34f26351149ed6b244b3154755f2a9e5
OmphileMatsobe <omphilejmatsobe@gmail.com>
2024-02-25 08:40:36 +0200

    New Files added

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BestBuyBuddy</title>
+    <meta name="description" content="we help you get the best online sales within your budget.">
+    <meta name="keyword" content="budget, sales, phones, electronics, books, games">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+    <nav>
+        <div></div>
+        <ul>
+            <li><a>Products</a></li>
+            <li><a>Explore</a></li>
+            <li><a>Resources</a></li>
+        </ul>
+        <div>Sign In<div>
+    </nav>
+    <header></header>
+    <main></main>
+    <footer></footer>
+
+    <script src="javascript/script.js"></script>
+</body>
+</html>
